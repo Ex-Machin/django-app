fe3841f2458fb22aa6e98de7d022d1f01a5ae439
Dmitry Mikhailyuk <grinpigrin@gmail.com>
2022-02-13 11:34:51 +0000

    app files

--- my_django_app/urls.py
+++ my_django_app/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from hello.views import index
+from entries.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', index),
+    path('entries/', index),
 ]
